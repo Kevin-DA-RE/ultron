3434e772bad38d679f6c161dabc5f7987e3ecade
Kevin <dare.kevin21000@gmail.com>
2024-02-26 23:03:47 +0100

    install quasar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import Layout from './layouts/Layout.vue'
 import router from './router/index'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
 
 const app = createApp(Layout)
 
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  })
+  
 app.use(router)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
     plugins: [
@@ -10,12 +11,12 @@ export default defineConfig({
         }),
         vue({
             template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+                transformAssetUrls
             },
         }),
+        quasar({
+            sassVariables: 'src/quasar-variables.sass'
+          })
     ],
     resolve: {
         alias: {
