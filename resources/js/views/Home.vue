8fbd750515dc69ae21b62e8d691e3807592cb1e6
Kevin <dare.kevin21000@gmail.com>
2024-03-10 00:50:34 +0100

    test send api movie

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <p>{{appelApi}}</p>
+    <p>{{ appelApi }}</p>
     <q-btn color="secondary" label="Prêt pour appel api"  @click="sendApi"/>
-    <h1>{{ backApi }}</h1>
-    <q-btn color="deep-orange" glossy label="Reset message api" @click="resetApi"/>
+    <q-card class="my-card">
+      <q-img :src="url">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          {{ title }}
+        </div>
+        <p>{{ synopsis }}</p>
+      </q-img>
+    </q-card>
   </div>
 </template>
 <script>
@@ -13,21 +19,31 @@ export default {
   data() {
     return {
       appelApi: "Test appel api !",
-      backApi: "Avant retour api"
+      title: '',
+      synopsis: '',
+      url : 'https://api.themoviedb.org/3/search/movie'
     }
   },
   methods: {
     async sendApi() {
-      try {
-        const response = await axios.get('http://127.0.0.1:8000/api');        
-        console.log(response.data);
-        this.backApi = response.data;
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error);
-      }
-    },
-    resetApi() {
-      this.backApi = "Avant retour api";
+        const response = await axios({
+                                        method: 'get',
+                                        url: this.url,
+                                        params: {
+                                          query: 'barbie',
+                                          include_adult: false,
+                                          language: 'fr-FR',
+                                          page: 1
+                                        },
+                                      headers:{
+                                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTFlMjdhNzVhZTY1ZTNjNDUxNjdlMmVkOTYwMmU3MSIsInN1YiI6IjY1ZThlZmEzM2Q3NDU0MDE3ZGI4MzczNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GNY6Ryp_gInMIzeoedzI7ooJHMdm1wX9YSTQyODot9s',
+                                        accept: 'application/json',
+                                        'Content-Type': 'application/json'
+
+                                      }})
+                                      .then(response => (response.data.results[0]));
+        console.log(response.title);
+        this.title = response.title;
     }
   }
 }
